3b3fd20c542e5ffa7c28fce3c3a4ab1e0ddfcd4b
Jelena Pletikosa <jellena.phy@gmail.com>
2018-08-14 19:42:02 +0200

    Internationalization initialized 🌐

--- public/client.js
+++ public/client.js
@@ -1,1 +1,63 @@
-console.log('§§§§§§§§§§§§§§ client script');
+console.log(`
+    §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
+    §§§§§§§ client listening §§§§§§§
+    §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
+    `);
+
+// *****************************************************************************
+// default browser language to the server
+// *****************************************************************************
+
+const browserLang = window.navigator.languages;
+console.log('§§§§§§§ detecting browserLang:\n', browserLang[1].slice(0, 2).toLowerCase());
+
+axios.post('/axios/browserLang', { lang : browserLang[1].slice(0, 2).toLowerCase() })
+    .then((response) => {
+        if (response.data.reload) window.location.reload();
+        console.log('§§§§§§§ axios post browserLang - req.body:\n', { lang : browserLang[1].slice(0, 2).toLowerCase() }, '\n\n');
+    });
+
+// *****************************************************************************
+// language menu listeners
+// *****************************************************************************
+
+$('#lang-partial').hide();
+
+$('#lang-nav').on('click', (e) => {
+    e.stopPropagation();
+    console.log('§§§§§§§ click on lang-nav\n\n');
+    $('#lang-partial').toggle();
+    console.log('§§§§§§§ toggling lang-partial\n\n');
+});
+
+$('#lang-partial').on('click', (e) => {
+    e.stopPropagation();
+    console.log('§§§§§§§ click on lang-partial - icon:\n', e.target.alt, '\n\n');
+
+    // *************************************************************************
+    // user selected language to the server
+    // *************************************************************************
+
+    axios.post('/axios/userLang', { lang : e.target.alt })
+        .then( () => {
+            window.location.reload();
+            console.log('§§§§ axios post userLang - req.body:\n', { lang : e.target.alt }, '\n\n');
+        });
+});
+
+// *****************************************************************************
+// body listeners
+// *****************************************************************************
+
+$('body').on('click', (e) => {
+    console.log('§§§§§§§ click on body\n\n');
+    if (e.target.id !== 'lang-partial' && $('#lang-partial').is(':visible')) {
+        console.log('§§§§ click outside of lang-partial\n\n');
+        $('#lang-partial').hide();
+        console.log('§§§§§ hiding lang-partial\n\n');
+    }
+});
+
+// *****************************************************************************
+//
+// *****************************************************************************
--- server.js
+++ server.js
@@ -3,6 +3,28 @@ console.log('\x1Bc');
 const express = require('express');
 const app = express();
 const hb = require('express-handlebars');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const fs = require('fs');
+
+// *****************************************************************************
+// requiring adequate translation json
+// *****************************************************************************
+
+let translationsList = [];
+const langFiles = fs.readdirSync(`${__dirname}/translations`);
+for (let i = 0; i < langFiles.length; i++) {
+    const stat = fs.statSync(`${__dirname}/translations/${langFiles[i]}`);
+    if (stat.isFile() && langFiles[i].indexOf('.json') !== -1) translationsList.push(langFiles[i]);
+}
+console.log('§§§§§§§ translationsList:\n', translationsList);
+
+const DEFAULT_LANG = 'en';
+const loadLang = (lang = DEFAULT_LANG) => {
+    const loaded = require(`./translations/${lang}.json`);
+    if (loaded) return loaded;
+    else return require(`./translations/${DEFAULT_LANG}.json`);
+};
 
 // *****************************************************************************
 // serving static files
@@ -11,29 +33,75 @@ const hb = require('express-handlebars');
 app.use(express.static(`${__dirname}/public`));
 
 // *****************************************************************************
+// body parser json
+// *****************************************************************************
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// *****************************************************************************
+// cookie session
+// *****************************************************************************
+
+app.use(cookieSession({
+    secret: '...my secret cookie session',
+    maxAge: 1000 * 60 * 60 * 24 * 14
+}));
+
+// *****************************************************************************
 // handlebars boilerplate
 // *****************************************************************************
 
-app.engine(
-    'handlebars',
-    hb({
-        defaultLayout: 'main'
-    })
-);
+app.engine('handlebars', hb({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // *****************************************************************************
-// routes
+// HttpRequests from the client
 // *****************************************************************************
 
-app.get('*', (req, res) => {
-    res.status(404);
-    res.render('home');
+app.get('/', (req, res) => {
+    console.log('§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§\n§§§§§§§ get app / ...rendering home view');
+    res.status(200);
+    if (req.session.lang) {
+        console.log('§§§§ req.session.lang defined:\n', req.session.lang, '\n');
+        res.render('home', {lang: loadLang(req.session.lang)});
+    } else {
+        console.log('§§§§ req.session.lang undefined\n');
+        res.render('home', {lang: loadLang()});
+    }
 });
+
+// *****************************************************************************
+// XMLHttpRequests from the client
+// *****************************************************************************
+
+app.post('/axios/browserLang', (req, res) => {
+    console.log('§§§§§§§ post axios browserLang - req.body:\n', req.body);
+    if (!req.session.lang && req.body.lang !== DEFAULT_LANG && translationsList.indexOf(req.body.lang) !== -1 ) {
+        req.session.lang === req.body.lang;
+        console.log('§§§§ !req.session.lang && browserLang != default && supported - reloading\n');
+        res.json ({ reload : true });
+    } else {
+        console.log('§§§§ req.session.lang || browserLang == default || not supported\n');
+        res.end();
+    }
+});
+
+app.post('/axios/userLang', (req, res) => {
+    console.log('§§§§§§§ post axios userLang - req.body:\n', req.body, '\n');
+    req.session.lang = req.body.lang;
+    res.end();
+
+});
+
 
 // *****************************************************************************
 // listening
 // *****************************************************************************
 
 app.listen(process.env.PORT || 8080,
-    console.log('§§§§§§§§§§§§§§ server listening...'));
+    console.log(`
+    §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
+    §§§§§§§§§§§§§§ server listening §§§§§§§§§§§§§§
+    §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
+    `));
